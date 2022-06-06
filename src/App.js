24b8461b3d5ecc50288f5512309a1a0c5c1ce704
NarutSH <narut.sh@gmail.com>
2022-06-06 15:35:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [qrCodeSrc, setQrCodeSrc] = useState("/assets/images/blank.jpg");
+
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [amount, setAmount] = useState();
+
+  const qrOptions = { type: "svg", color: { dark: "#000", light: "#fff" } };
+
+  const styles = {
+    formContainer: {
+      width: "500px",
+      margin: "10px auto",
+    },
+    qrCode: {
+      width: "200px",
+      height: "200px",
+    },
+  };
+
+  const onGenerateSrc = () => {
+    const src = `https://promptpay.io/${mobileNumber}/${amount}.png`;
+    setQrCodeSrc(src);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center py-4">
+      <img src={qrCodeSrc} className="img-fluid" style={styles.qrCode} />
+      <div style={styles.formContainer}>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            value={mobileNumber}
+            onChange={(ev) => setMobileNumber(ev.target.value)}
+          />
+          <label>เบอร์โทรศัพท์ หรือ เลขบัตรประชาขน</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            value={amount}
+            onChange={(ev) => setAmount(+ev.target.value)}
+          />
+          <label>จำนวนเงิน</label>
+        </div>
+      </div>
+
+      <button className="btn btn-success" onClick={onGenerateSrc}>
+        Submit
+      </button>
     </div>
   );
-}
+};
 
 export default App;
